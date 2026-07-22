59c3545076804e555ab22c1eb5dba069bde332f1
Amiin Abdalla <amiinabdalla909@gmail.com>
2026-07-22 22:04:21 +0300

    Add SQL schema script for SOM-ICT Project

---
+++ SOM-ICT PROJECT.sql
@@ -0,0 +1,11 @@
+
+
+Create database DataAnalyticsDB;
+
+USE DataAnalyticsDB;
+
+
+select * from  [Dataset for data Analytics (2)];
+
+select  top  10 CustomerID , ReferralSource from [Dataset for data Analytics (2)]
+
